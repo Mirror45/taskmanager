f12fb394ed3cccd9da093f8d31778063d4b7b190
Mirror <yllikano@gmail.com>
2025-02-27 04:35:57 +0200

    feat: add TaskFilter component and styles
    - Create TaskFilter component
    
    - Add styles for TaskFilter using CSS Modules
    
    - Render TaskFilter in TaskBoard component

--- src/components/TaskBoard/TaskBoard.tsx
+++ src/components/TaskBoard/TaskBoard.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import TaskList from '../TaskList/TaskList';
 import styles from './TaskBoard.module.css';
+import TaskFilter from '../TaskFilter/TaskFilter';
 
 const TaskBoard: React.FC = () => {
   return (
     <div className={styles.taskBoard}>
+      <TaskFilter />
       <TaskList />
     </div>
   );
