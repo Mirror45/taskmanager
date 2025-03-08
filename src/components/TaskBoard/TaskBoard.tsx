b5c2401735b2668d7469587fdb1b3c5314de8305
Mirror <yllikano@gmail.com>
2025-03-08 15:16:31 +0200

    refactor: update TaskBoard

--- src/components/TaskBoard/TaskBoard.tsx
+++ src/components/TaskBoard/TaskBoard.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import TaskList from '../TaskList/TaskList';
 import TaskSort from '../TaskSort/TaskSort';
-import AddNewTask from '../NewTask/AddNewTask';
+import AddNewTask from '../AddNewTask/AddNewTask';
 
 const TaskBoard: React.FC = () => {
   return (
