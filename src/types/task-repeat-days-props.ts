5ee1ce23293cb226bd02bda7cf06bd94a5be4746
Mirror <yllikano@gmail.com>
2025-03-17 16:58:24 +0200

    feat: add toggle repeat in TaskRepeatDays

--- src/types/task-repeat-days-props.ts
+++ src/types/task-repeat-days-props.ts
@@ -2,4 +2,6 @@ export interface TaskRepeatDaysProps {
   selectedDays: string[];
   // eslint-disable-next-line no-unused-vars
   onDayChange: (day: string) => void;
+  isRepeatVisible: boolean;
+  onToggleRepeat: () => void;
 }
